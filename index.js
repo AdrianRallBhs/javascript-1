964550d523ea205bf7114b556bef957e3aab6e0c
Adrian Rall <adrian@asda4.de>
2022-09-28 16:04:26 +0200

    push and test javascript action Should create a file

---
+++ index.js
@@ -0,0 +1,24 @@
+const core = require ("@actions/core");
+
+const firstGreeting = core.getInput("first-greeting");
+const secondGreeting = core.getInput("second-greeting");
+const lastGreeting = core.getInput("last-greeting");
+
+async function run() {
+    try {
+        if(firstGreeting) {
+            core.setOutput(("last one greeted", firstGreeting));
+        } else if (secondGreeting) {
+            core.setOutput(("last one greeted", secondGreeting));
+        } else if (lastGreeting) {
+            core.setOutput(("last one greeted", lastGreeting));
+        }
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run();
+console.log("The first one to be greeted was ${firstGreeting}!");
+console.log("The second one to be greeted was ${secondGreeting}!");
+console.log("The third one to be greeted was ${lastGreeting}!");
